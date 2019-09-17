2c6967fa9584d4727a6f22b0520a84dccaf47d57
MasiaVittorio <masiavivittorio@gmail.com>
2019-09-17 18:22:15 +0200

    naming fix

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,15 @@ import 'package:counter_spell_new/widgets/scaffold/homepage.dart';
 import 'package:flutter/material.dart';
 import 'package:sidereus/bloc/bloc_provider.dart';
 
-void main() => runApp(ScryWalker());
+void main() => runApp(CounterSpell());
 
-class ScryWalker extends StatefulWidget {
+class CounterSpell extends StatefulWidget {
   // This widget is the root of your application.
   @override
-  _ScryWalkerState createState() => _ScryWalkerState();
+  _CounterSpellState createState() => _CounterSpellState();
 }
 
-class _ScryWalkerState extends State<ScryWalker> {
+class _CounterSpellState extends State<CounterSpell> {
   CSBloc bloc;
 
   @override
